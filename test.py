8bf74d5b63cc8dc90aefc50bef7ac926ec7a7373
Mustafa Kamal <mustfakmalik@gmail.com>
2023-05-06 22:13:04 +0500

    fan not working so modify code to test.py

--- test.py
+++ test.py
@@ -1,46 +1,48 @@
 import RPi.GPIO as GPIO
 import time
 
-# Set the GPIO mode
-GPIO.setmode(GPIO.BCM)
-
-# Define the GPIO pins for the LED and fan
+# Pin numbers for LED and fan
 LED_PIN = 18
 FAN_PIN = 12
 
-# Set up the LED and fan pins as output
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED_PIN, GPIO.OUT)
 GPIO.setup(FAN_PIN, GPIO.OUT)
 
-# Set up PWM for the fan pin with a frequency of 25 kHz
-fan_pwm = GPIO.PWM(FAN_PIN, 25000)
+# PWM for controlling fan speed
+pwm = GPIO.PWM(FAN_PIN, 100)
+pwm.start(0)
 
-# Blink the LED and control the fan speed
+def turn_on_led():
+    GPIO.output(LED_PIN, GPIO.HIGH)
+
+def turn_off_led():
+    GPIO.output(LED_PIN, GPIO.LOW)
+
+def set_fan_speed(speed):
+    if speed == "low":
+        pwm.ChangeDutyCycle(33)
+    elif speed == "medium":
+        pwm.ChangeDutyCycle(66)
+    elif speed == "high":
+        pwm.ChangeDutyCycle(100)
+
 try:
     while True:
-        # Turn on the LED and set the fan speed to low (25% duty cycle)
-        GPIO.output(LED_PIN, GPIO.HIGH)
-        fan_pwm.start(25)
-        time.sleep(2)
-
-        # Turn off the LED and set the fan speed to medium (50% duty 
-cycle)
-        GPIO.output(LED_PIN, GPIO.LOW)
-        fan_pwm.ChangeDutyCycle(50)
-        time.sleep(2)
-
-        # Turn on the LED and set the fan speed to high (75% duty cycle)
-        GPIO.output(LED_PIN, GPIO.HIGH)
-        fan_pwm.ChangeDutyCycle(75)
-        time.sleep(2)
-
-        # Turn off the LED and turn off the fan
-        GPIO.output(LED_PIN, GPIO.LOW)
-        fan_pwm.stop()
-        time.sleep(2)
+        # Test LED light and fan speed
+        turn_on_led()
+        set_fan_speed("low")
+        time.sleep(5)
+        turn_off_led()
+        set_fan_speed("medium")
+        time.sleep(5)
+        turn_on_led()
+        set_fan_speed("high")
+        time.sleep(5)
+        turn_off_led()
+        set_fan_speed("low")
+        time.sleep(5)
 
 except KeyboardInterrupt:
-    # Stop the fan and clean up the GPIO when the script is stopped
-    fan_pwm.stop()
+    pwm.stop()
     GPIO.cleanup()
-

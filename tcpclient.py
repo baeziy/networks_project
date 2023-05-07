8f766bd9ab3abb592f698c466c24ea54b7e15d6b
Mustafa Kamal <mustfakmalik@gmail.com>
2023-05-07 15:20:54 +0500

    modify tcpclient.py

--- tcpclient.py
+++ tcpclient.py
@@ -1,19 +1,17 @@
 import socket
 
 # Set up the client
-TCP_IP = 'your_raspberry_pi_ip_address'  # Replace with the actual IP 
-address of your Raspberry Pi
+TCP_IP = '192.168.18.70'  # Replace with the actual IP 
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
 
 # Create a socket and connect to the server
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((TCP_IP, TCP_PORT))
-
+print("connection established")
 while True:
     # Read user input
-    message = input("Enter 'ON' to turn the LED on or 'OFF' to turn it off 
-(type 'exit' to quit): ")
+    message = input("Enter 'ON' to turn the LED on or 'OFF' to turn it off (type 'exit' to quit): ")
 
     if message.lower() == 'exit':
         break

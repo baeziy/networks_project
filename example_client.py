e52af37da09b921a931aa87585b5d376d07e6974
Mustafa Kamal <mustfakmalik@gmail.com>
2023-05-07 20:03:12 +0500

    modify example code

--- example_client.py
+++ example_client.py
@@ -1,18 +1,27 @@
 import socket
 
-# define the IP address and port number
-SERVER_IP = '192.168.18.70'
-SERVER_PORT = 1234
+host = '192.168.18.70'  # IP address of machine B
+port = 1234  # Port number to test the connection on
 
-# create a socket object
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# connect to the server
-client_socket.connect((SERVER_IP, SERVER_PORT))
-
-# send data to the server
-data = "Hello from PC".encode()
-client_socket.sendall(data)
-
-# close the connection
-client_socket.close()
+try:
+    # create a socket object and connect to the server
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((host, port))
+    print(f"Connection to {host}:{port} successful!")
+    
+    # send a test message to the server
+    message = "Hello from machine A"
+    sock.sendall(message.encode())
+    print(f"Sent message: {message}")
+    
+    # receive the server's response and print it
+    data = sock.recv(1024)
+    print(f"Received response: {data.decode()}")
+    
+    # close the socket connection
+    sock.close()
+    
+except ConnectionRefusedError:
+    print(f"Connection to {host}:{port} refused.")
+except Exception as e:
+    print(f"An error occurred: {e}")
--- example_server.py
+++ example_server.py
@@ -1,27 +1,32 @@
 import socket
 
-# define the IP address and port number
-SERVER_IP = '0.0.0.0'
-SERVER_PORT = 1234
+host = '0.0.0.0'  # listen on all available network interfaces
+port = 1234  # Port number to test the connection on
 
-# create a socket object
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# bind the socket object to the IP address and port number
-server_socket.bind((SERVER_IP, SERVER_PORT))
-
-# listen for incoming connections
-server_socket.listen()
-
-# accept the connection
-client_socket, client_address = server_socket.accept()
-
-# receive data from the client
-data = client_socket.recv(1024)
-
-# print the received data
-print("Received data:", data.decode())
-
-# close the connection
-client_socket.close()
-server_socket.close()
+try:
+    # create a socket object and bind it to the specified port
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((host, port))
+    
+    # listen for incoming connections (maximum 1 connection at a time)
+    sock.listen(1)
+    print(f"Server listening on {host}:{port}")
+    
+    # wait for a client to connect and accept the connection
+    conn, addr = sock.accept()
+    print(f"Connected by {addr}")
+    
+    # receive the client's message and print it
+    data = conn.recv(1024)
+    print(f"Received message: {data.decode()}")
+    
+    # send a response to the client
+    response = "Hello from machine B"
+    conn.sendall(response.encode())
+    print(f"Sent response: {response}")
+    
+    # close the socket connection
+    conn.close()
+    
+except Exception as e:
+    print(f"An error occurred: {e}")

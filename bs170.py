06a8ad53a6649f663bb60a45de9054486c2ef3b4
Mustafa Kamal <mustfakmalik@gmail.com>
2023-05-08 18:24:29 +0500

    create bs170.py

--- bs170.py
+++ bs170.py
@@ -4,16 +4,12 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-led_pin = 18  # Connected to the Gate of the MOSFET
+LED_PIN = 18
 
-GPIO.setup(led_pin, GPIO.OUT)
+GPIO.setup(LED_PIN, GPIO.OUT)
 
-# Turn the LED on and off
-try:
-    while True:
-        GPIO.output(led_pin, GPIO.HIGH)
-        time.sleep(1)
-        GPIO.output(led_pin, GPIO.LOW)
-        time.sleep(1)
-except KeyboardInterrupt:
-    GPIO.cleanup()
+while True:
+    GPIO.output(LED_PIN, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(LED_PIN, GPIO.LOW)
+    time.sleep(1)

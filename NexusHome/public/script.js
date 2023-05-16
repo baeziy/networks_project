c9da9568615e963083fccef10eb8bbf68a13aa29
Mustafa Kamal <mustfakmalik@gmail.com>
2023-05-16 18:36:42 +0500

    possible connection of front-end with backend

--- NexusHome/public/script.js
+++ NexusHome/public/script.js
@@ -1,10 +1,197 @@
-// Connect to WebSocket
-const ws = new WebSocket('ws://raspberrypi.local:3000');
+var speed = 0;
+var prev_speed = 0;
+var currentScale = 1;
 
-// Send fan speed to server when button is clicked
-document.querySelectorAll('.fan-speed-button').forEach(button => {
-    button.addEventListener('click', () => {
-        const speed = button.dataset.speed;
-        ws.send(speed);
-    });
-});
+function state()
+{
+    let state = 'off'
+    
+    if (speed > 0 && speed <= 50) 
+    {
+        document.getElementById("mode").innerHTML = "LOW"
+        state = 'low';
+    }
+
+    else if (speed > 50 && speed <= 120) 
+    {
+        document.getElementById("mode").innerHTML = "MED"
+        state = 'medium';
+    }
+
+    else if (speed > 120) 
+    {
+        document.getElementById("mode").innerHTML = "HIGH"
+        state = 'high';
+    }
+
+    else
+    {
+        document.getElementById("mode").innerHTML = "OFF"
+    }
+
+    sendFanSpeed(state);
+
+}
+
+function sendFanSpeed(speed) {
+    fetch('/state/fan/' + speed)
+        .then((response) => {
+            return response.text();
+        })
+        .then((text) => console.log(text))
+    }
+
+function killswitch()
+{
+    if (speed > 0)
+    {
+        while (speed != 0) 
+        {
+            speed = speed - 10;
+            addClass();
+            changeActive();
+            currentScale = currentScale - 1;
+        }
+        document.getElementById("state_off").innerHTML = "Fan Off"
+        document.getElementById("mode").innerHTML = "OFF"
+    }
+
+    document.getElementById("switch").className = "red"
+    document.getElementById("switch").innerHTML = "OFF"
+    document.getElementById("light").className = "dark"
+}
+
+function increase()
+{
+    if (speed < 180)
+    {
+        if (speed == 0)
+        {
+            while (speed != 50) 
+            {
+                speed = speed + 10;
+                addClass();
+                currentScale = currentScale + 1;
+                changeActive();
+            }
+        }
+
+        else if (speed == 50)
+        {
+            while (speed != 120) 
+            {
+                speed = speed + 10;
+                addClass();
+                currentScale = currentScale + 1;
+                changeActive();
+            }
+        }
+
+        else if (speed == 120)
+        {
+            while (speed != 180) 
+            {
+                speed = speed + 10;
+                addClass();
+                currentScale = currentScale + 1;
+                changeActive();
+            }
+        }
+
+        if (speed > 0)
+        {
+            document.getElementById("state_off").innerHTML = "Decrease Speed"
+        }
+
+        state()
+
+    }
+}
+
+function decrease()
+{
+    if (speed > 0)
+    {
+        if (speed == 50)
+        {
+            while (speed != 0) 
+            {
+                speed = speed - 10;
+                addClass();
+                changeActive();
+                currentScale = currentScale - 1;
+            }
+        }
+
+        else if (speed == 120)
+        {
+            while (speed != 50) 
+            {
+                speed = speed - 10;
+                addClass();
+                changeActive();
+                currentScale = currentScale - 1;
+            }
+        }
+
+        else if (speed == 180)
+        {
+            while (speed != 120) 
+            {
+                speed = speed - 10;
+                addClass();
+                changeActive();
+                currentScale = currentScale - 1;
+            }
+        }
+        
+        if (speed == 0)
+        {
+            document.getElementById("state_off").innerHTML = "Fan Off"
+        }
+
+        state()
+    }
+}
+
+function addClass()
+{
+    var newClass = "speed-" + speed;
+    var prevClass = "speed-" + prev_speed;
+    var el = document.getElementsByClassName("needle_wrapper")[0];
+
+    if (el.classList.contains(prevClass))
+    {
+        el.classList.remove(prevClass);
+        el.classList.add(newClass);
+    }
+
+    prev_speed = speed;
+}
+
+function changeActive()
+{
+    var tempClass = "speedscale-" + currentScale;
+    var el = document.getElementsByClassName(tempClass)[0];
+    el.classList.toggle("active");
+}
+
+function _switch()
+{
+    var state = document.getElementById("switch").innerHTML;
+    
+    if (state == "OFF")
+    {
+        document.getElementById("switch").className = "green"
+        document.getElementById("switch").innerHTML = "ON"
+        document.getElementById("light").className = "light"
+    }
+
+    else if (state == "ON")
+    {
+        document.getElementById("switch").className = "red"
+        document.getElementById("switch").innerHTML = "OFF"
+        document.getElementById("light").className = "dark"
+    }
+
+}
